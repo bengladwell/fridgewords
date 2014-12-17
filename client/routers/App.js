5ff2d72fbe3f90298682f8507778a6652f1bd057
Ben Gladwell <bengladwell@gmail.com>
2014-12-16 20:51:19 -0500

    needs templates

--- client/routers/App.js
+++ client/routers/App.js
@@ -2,12 +2,27 @@
   "use strict";
 
   var _ = window._,
-    Backbone = window.Backbone;
+    Backbone = window.Backbone,
+    GameView = require('../views/Game'),
+    SettingsView = require('../views/Settings');
 
   module.exports = Backbone.Router.extend({
 
     initialize: function (options) {
-      _.extend(this, options);
+      _.extend(this, _.pick(options, 'layout'));
+    },
+
+    routes: {
+      "": "game",
+      "settings": "settings"
+    },
+
+    game: function () {
+      this.layout.setView(new GameView());
+    },
+
+    settings: function () {
+      this.layout.setView(new SettingsView());
     }
 
   });
--- client/views/Layout.js
+++ client/views/Layout.js
@@ -4,6 +4,15 @@
   var Backbone = window.Backbone;
 
   module.exports = Backbone.View.extend({
+
+    setView: function (view) {
+      if (this._view) {
+        this._view.remove();
+      }
+      this._view = view;
+      this.$('.outlet').html(view.render().el);
+    }
+
   });
 
 }());
