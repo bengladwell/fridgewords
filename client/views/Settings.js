ae000e3420ba9bcd2fc77f630c17e2b7c56af873
Ben Gladwell <bengladwell@gmail.com>
2014-12-27 15:32:50 -0500

    bulk add words on settings page

--- client/views/Settings.js
+++ client/views/Settings.js
@@ -1,13 +1,33 @@
 (function () {
   "use strict";
 
-  var Backbone = window.Backbone,
+  var _ = window._,
+    Backbone = window.Backbone,
     template = require('../templates/settings'),
+    AvailableWordsCollection = require('../collections/AvailableWords'),
     View;
 
   View = Backbone.View.extend({
 
+    tagName: 'form',
+
     className: 'settings',
+
+    events: {
+      submit: function (e) {
+        e.preventDefault();
+
+        var words = new AvailableWordsCollection(_.map(this.$('textarea').val().split(/\n/), function (token) {
+          return { label: token.trim() };
+        }));
+
+        Backbone.$.when(words.map(function (word) {
+          word.save();
+        })).then(function () {
+          Backbone.history.navigate("", {trigger: true});
+        });
+      }
+    },
 
     render: function () {
       this.$el.html(template());
